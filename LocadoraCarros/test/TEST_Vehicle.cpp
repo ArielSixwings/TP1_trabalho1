2f949a29e2031dbe67af294214a0b4b3eddbf9cb
ariel <arielserafi@gmail.com>
2018-11-18 19:56:59 -0200

    mais testes

--- LocadoraCarros/test/TEST_Vehicle.cpp
+++ LocadoraCarros/test/TEST_Vehicle.cpp
@@ -11,12 +11,12 @@ TEST_CASE("ConstructorTest") {
 	REQUIRE(car.Color).compare("RED") == 0);
 	REQUIRE(car.TypeFuel).compare("ALCOHOL") == 0);
     REQUIRE(car.Chassi).compare("3KI8S30KMN14IOM40") == 0);
-    REQUIRE(car.Year).compare(2015) == 0);
-    REQUIRE(car.howmanydays).compare(4) == 0);
-    REQUIRE(car.Distance).compare(923844.0) == 0);
+    REQUIRE(car.Year == 2015);
+    REQUIRE(car.howmanydays) == 4);
+    REQUIRE(car.Distance == 923844.0;
     REQUIRE(car.renavam.compare("00184729834") == 0);
     REQUIRE(car.Model).compare("Fusion") == 0);
     REQUIRE(car.Brand).compare("Ford") == 0);
-    REQUIRE(car.Priceperday).compare(103.5) == 0);
-    REQUIRE(car.Priceperperiod).compare(93.15) == 0);
+    REQUIRE(car.Priceperday == 103.5);
+    REQUIRE(car.Priceperperiod == 93.15);
 }
